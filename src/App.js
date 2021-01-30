1f4a7c105e9bedb69969fcfcf73c769458b20661
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-29 19:40:25 -0800

    Get info from my api https://github.com/bryantt23/blog-list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import React, { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    async function fetchData() {
+      // You can await here
+      const blogs = await blogService.getAll();
+      const blogsArr = Object.entries(blogs).map(obj => obj[1]);
+      setBlogs(blogsArr);
+    }
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,11 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from 'axios';
+const baseUrl = 'http://localhost:3001/api/blogs';
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+const getAll = async () => {
+  const request = await axios.get(baseUrl);
+  const data = await request.data;
+  return data;
+  // return request.then(response => response.data);
+};
 
-export default { getAll }
+export default { getAll };
