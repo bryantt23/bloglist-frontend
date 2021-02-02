2d2e16b154efce7423d64b36671a133d4510013b
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-01 23:02:05 -0800

    Toggle visibility of posts
    https://fullstackopen.com/en/part5/props_children_and_proptypes

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  const [expanded, setExpanded] = useState(false);
+
+  if (!expanded) {
+    return (
+      <div style={blogStyle}>
+        Title: {blog.title}{' '}
+        <button onClick={() => setExpanded(true)}>view</button>
+      </div>
+    );
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <p> Title: {blog.title}</p>
+      <p> Url: {blog.url}</p>
+      <p>
+        {' '}
+        Likes: {blog.likes}
+        <button>like</button>
+      </p>
+      <p> Author: {blog.author}</p>
+      <button onClick={() => setExpanded(false)}>hide</button>
+    </div>
+  );
+};
+export default Blog;
